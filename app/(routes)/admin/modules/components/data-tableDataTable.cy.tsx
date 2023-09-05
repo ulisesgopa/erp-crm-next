39fb88fafd8c6f4023442b82145f5faeebd84676
saashqdev <saashqdev@gmail.com>
2023-09-05 15:00:52 -0400

    update Cypress

--- app/(routes)/admin/modules/components/data-tableDataTable.cy.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { DataTable } from './data-table'
-
-describe('<DataTable />', () => {
-  it('renders', () => {
-    // see: https://on.cypress.io/mounting-react
-    cy.mount(<DataTable />)
-  })
-})
