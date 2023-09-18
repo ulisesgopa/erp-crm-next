acc3cf0b2cf154b7b40cd7343018a7205f6553c2
saashqdev <saashqdev@gmail.com>
2023-09-18 12:09:51 -0400

    Update - CRM - Kanban - completed
    Update - Project - Kanban
    - added ... menu

--- app/[locale]/(routes)/crm/dashboard/page.tsx
+++ app/[locale]/(routes)/crm/dashboard/page.tsx
@@ -3,7 +3,6 @@ import Container from "../../components/ui/Container";
 import { getSaleStages } from "@/actions/crm/get-sales-stage";
 import CRMKanban from "./_components/CRMKanban";
 import { getOpportunities } from "@/actions/crm/get-opportunities";
-import CRMKanbanServer from "./_components/CRMKanbanServer";
 
 const CrmDashboardPage = async () => {
   const salesStages = await getSaleStages();
@@ -12,7 +11,7 @@ const CrmDashboardPage = async () => {
   return (
     <Container
       title="CRM Dashboard"
-      description="In development. After this compoment is finished, there will be a optimistic update of the data."
+      description="In development. After this compoment is finished, there will be an optimistic update of the data."
     >
       <div className="w-full h-full  overflow-hidden">
         <CRMKanban salesStages={salesStages} opportunities={opportunities} />
