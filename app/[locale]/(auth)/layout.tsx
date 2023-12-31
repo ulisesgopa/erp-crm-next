f8b05d7c92ee508b82161b79c223552ed61d8d6a
SaasHQ <saashqdev@gmail.com>
2023-12-31 16:25:08 -0500

    Update - Build count info - added 2 actions for fetching data from Github - added build info counter to Sidebar component - updated .env.local.example - new strings for Github

--- app/[locale]/(auth)/layout.tsx
+++ app/[locale]/(auth)/layout.tsx
@@ -3,11 +3,11 @@ import Link from "next/link";
 import { notFound } from "next/navigation";
 import { createTranslator } from "next-intl";
 import { GithubIcon, Star } from "lucide-react";
-import "primeicons/primeicons.css";
 
 import "@/app/[locale]/globals.css";
 import { ThemeToggle } from "@/components/ThemeToggle";
 import Footer from "@/app/[locale]/(routes)/components/Footer";
+import getGithubRepoStars from "@/actions/github/get-repo-stars";
 
 type Props = {
   params: { locale: string };
@@ -32,9 +32,7 @@ export async function generateMetadata({ params: { locale } }: Props) {
 
 const AuthLayout = async ({ children }: { children: React.ReactNode }) => {
   //Get github stars from github api
-  const github = await axios
-    .get(`https://api.github.com/repos/saashqdev/saashq`)
-    .then((res) => res.data);
+  const githubStars = await getGithubRepoStars();
 
   return (
     <div className="flex flex-col justify-center items-center h-screen w-full">
@@ -46,14 +44,14 @@ const AuthLayout = async ({ children }: { children: React.ReactNode }) => {
           <i className="pi pi-discord w-5 h-5" />
         </Link>
         <Link
-          href={"https://github.com/saashqdev/saashq"}
+          href={process.env.NEXT_PUBLIC_GITHUB_REPO_URL || "#"}
           className=" border rounded-md p-2"
         >
           <GithubIcon className="w-5 h-5" />
         </Link>
         <div className="flex items-center border rounded-md p-2">
           <span className="sr-only">Github stars</span>
-          {github?.stargazers_count}
+          {githubStars}
           <Star className="w-4 h-4" />
         </div>        
         <ThemeToggle />
