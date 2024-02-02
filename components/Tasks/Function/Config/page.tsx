93a71b43ae36a46331448b543f80a7ab3421d3b8
SaasHQ <saashqdev@gmail.com>
2024-02-02 16:01:20 -0500

    Completed client-side form submit functionality

--- components/Tasks/Function/Config/page.tsx
+++ components/Tasks/Function/Config/page.tsx
@@ -7,7 +7,6 @@ import { useForm } from 'react-hook-form';
 import { z } from 'zod';
 
 import { Button } from "@/components/ui/button";
-import { Badge } from "@/components/ui/badge";
 import ParamsMonaco from './ParamsMonaco/ParamsMonaco';
 import ExecMonaco from './ExecMonaco/ExecMonaco';
 import { useReactFlow } from 'reactflow';
@@ -62,16 +61,18 @@ interface Props {
   id: string;
 }
 
-const FunctionConfigPanel: FC<Props> = ({ initialValue, deleteNode, id }) => {
+const FunctionConfigPanel: FC<Props> = ({ onSubmit, initialValue, deleteNode, id }) => {
   const [isLoading] = useState<boolean>(false);
-  const { toast } = useToast();  
+  const { toast } = useToast()
+
+  const [ openConfigPanel, setOpenConfigPanel ] = useState<boolean>(false);  
   const { getNodes } = useReactFlow();
   const [paramsEditorError, setParamsEditorError] = useState<string | null>(null);
   const [execEditorError, setExecEditorError] = useState<string | null>(null);
   const [labelUniqueError, setLabelUniqueError] = useState<string | null>(null);
   const [activeStep, setActiveStep] = useState<number>(0);
 
-  const { formState, watch, setValue } = useForm<FunctionConfigSchema>({
+  const { handleSubmit, watch, setValue } = useForm<FunctionConfigSchema>({
     resolver: zodResolver(functionConfigSchema),
     values: {
       label: initialValue?.label ?? '',
@@ -95,6 +96,21 @@ async function handler(){
 
   const form = useForm<FunctionConfigSchema>({
     resolver: zodResolver(functionConfigSchema),
+    values: {
+      label: initialValue?.label ?? '',
+      params: initialValue?.params ?? {},
+      exec: initialValue?.exec ?? '',
+      execTs:
+        initialValue?.execTs ??
+        `
+/**
+ * @see {@link https://workflow-engine-docs.pages.dev/docs/tasks/function_task}
+*/
+async function handler(){
+  return {};
+}
+      `,
+    },          
   });  
 
   useEffect(() => {
@@ -126,120 +142,145 @@ async function handler(){
     setExecEditorError(() => error);
   };
 
+  const submitHandler = handleSubmit(async (value: z.infer<typeof functionConfigSchema>) => {
+    onSubmit(value);
+    toast({
+      title: "Success",
+      description: "Config changed successfully."
+    })
+    handleConfigPanelClose();
+  });
+
+  const handleConfigPanelOpen = () => {
+    setOpenConfigPanel(() => true);
+  };
+
+  const handleConfigPanelClose = () => {
+    setOpenConfigPanel(() => false);
+  };  
+
   return (
     <>
-      <Badge variant="destructive" className="mr-3">
-        {
-          Object.keys(formState.errors).length +
-          (labelUniqueError ? 1 : 0) +
-          (execEditorError ? 1 : 0) +
-          (paramsEditorError ? 1 : 0)
-        }
-      </Badge>
-      <Sheet>
+      <Sheet open={openConfigPanel} onOpenChange={setOpenConfigPanel}>
         <Form {...form}>
-          <SheetTrigger asChild>
-            <Button variant="outline">Configure</Button>
-          </SheetTrigger>
-          <SheetContent className="sm:max-w-[540px]">
-            <SheetHeader>
-              <SheetTitle>{[initialValue?.label, 'Configuration'].join(' ')}</SheetTitle>
-                <SheetDescription>
-                  Make changes to Function Configuration panel.
-                </SheetDescription>
-              </SheetHeader>
-            <Separator className="mt-6" />
-            <div className="grid gap-4 py-4">
-              {activeStep === 0 && (
-                <>
-                  <FormField
-                    control={form.control}
-                    name="label"
-                    render={({ field }) => (
-                      <FormItem>
-                        <FormLabel>Label</FormLabel>
-                        <FormControl>
-                          <Input
-                            disabled={isLoading}
-                            placeholder="Name of the Task"
-                            {...field}
-                          />
-                        </FormControl>
-                      <FormMessage />
-                      </FormItem>
+          <form onSubmit={form.handleSubmit(submitHandler as any)}>          
+            <SheetTrigger asChild>
+              <Button variant="outline" onClick={handleConfigPanelOpen}>
+                Configure
+                <span>
+                  {Object.keys(form?.formState.errors).length > 0 ? (
+                    <span className="absolute bg-red-500 text-red-100 px-2 py-1 text-xs font-bold rounded-full -top-2 -right-2">
+                      {
+                        Object.keys(form?.formState.errors).length +
+                        (labelUniqueError ? 1 : 0) +
+                        (execEditorError ? 1 : 0) +
+                        (paramsEditorError ? 1 : 0)
+                      }
+                    </span>  
+                  ) : null}
+                </span>            
+              </Button>
+            </SheetTrigger>
+            <SheetContent className="sm:max-w-[540px]">
+              <SheetHeader>
+                <SheetTitle>{[initialValue?.label, 'Configuration'].join(' ')}</SheetTitle>
+                  <SheetDescription>
+                    Make changes to Function Configuration panel.
+                  </SheetDescription>
+                </SheetHeader>
+              <Separator className="mt-6" />
+              <div className="grid gap-4 py-4">
+                {activeStep === 0 && (
+                  <>
+                    <FormField
+                      control={form.control}
+                      name="label"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>Label</FormLabel>
+                          <FormControl>
+                            <Input
+                              disabled={isLoading}
+                              placeholder="Name of the Task"
+                              {...field}
+                            />
+                          </FormControl>
+                        <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                    <FormLabel>Params</FormLabel>
+                    {paramsEditorError && (
+                      <div className="flex gap-2">
+                        <FormLabel className="text-red-600">{paramsEditorError}</FormLabel>
+                      </div>
                     )}
-                  />
-                  <FormLabel>Params</FormLabel>
-                  {paramsEditorError && (
-                    <div className="flex gap-2">
-                      <FormLabel className="text-red-600">{paramsEditorError}</FormLabel>
-                    </div>
-                  )}
-                  <ParamsMonaco
-                    initialValue={JSON.stringify(paramsObjectValue, undefined, 4)}
-                    setValue={setValue}
-                    setError={handleParamsEditorError}
-                  />
+                    <ParamsMonaco
+                      initialValue={JSON.stringify(paramsObjectValue, undefined, 4)}
+                      setValue={setValue}
+                      setError={handleParamsEditorError}
+                    />
+                    <Button
+                      type="button"
+                      variant="secondary"
+                      onClick={() => {
+                        setActiveStep(() => 1);
+                      }}
+                    >
+                      Next&nbsp; <ChevronRight />
+                    </Button>
+                  </>
+                )}
+                {activeStep === 1 && (
+                  <>
+                    <FormLabel>Function</FormLabel>
+                    {execEditorError && (
+                      <div className="flex gap-2">
+                        <FormLabel className="text-red-600">{execEditorError}</FormLabel>
+                      </div>
+                    )}
+                    <ExecMonaco
+                      initialValue={execObjectValue}
+                      setValue={setValue}
+                      setError={handleExecEditorError}
+                      params={paramsObjectValue}
+                    />
+                    <Button
+                      variant="secondary"
+                      onClick={() => {
+                        setActiveStep(() => 0);
+                      }}
+                    >
+                      <ChevronLeft /> &nbsp;Prev
+                    </Button>
+                  </>
+                )}
+                <SheetFooter>
+                  <SheetClose asChild>
+                    <Button 
+                      type="submit"
+                      onClick={() => {
+                        toast({
+                          title: "Success",
+                          description: "Task changed successfully."
+                        })
+                      }}                  
+                    >
+                      Submit
+                    </Button>
+                  </SheetClose>
                   <Button
-                    type="button"
-                    variant="secondary"
+                    variant="destructive"
                     onClick={() => {
-                      setActiveStep(() => 1);
+                      deleteNode();
                     }}
                   >
-                    Next&nbsp; <ChevronRight />
+                    Delete Task
                   </Button>
-                </>
-              )}
-              {activeStep === 1 && (
-                <>
-                  <FormLabel>Function</FormLabel>
-                  {execEditorError && (
-                    <div className="flex gap-2">
-                      <FormLabel className="text-red-600">{execEditorError}</FormLabel>
-                    </div>
-                  )}
-                  <ExecMonaco
-                    initialValue={execObjectValue}
-                    setValue={setValue}
-                    setError={handleExecEditorError}
-                    params={paramsObjectValue}
-                  />
-                  <Button
-                    variant="secondary"
-                    onClick={() => {
-                      setActiveStep(() => 0);
-                    }}
-                  >
-                    <ChevronLeft /> &nbsp;Prev
-                  </Button>
-                </>
-              )}
-              <SheetFooter>
-                <SheetClose asChild>
-                  <Button 
-                    type="submit"
-                    onClick={() => {
-                      toast({
-                        title: "Success",
-                        description: "Task changed successfully."
-                      })
-                    }}                  
-                  >
-                    Submit
-                  </Button>
-                </SheetClose>
-                <Button
-                  variant="destructive"
-                  onClick={() => {
-                    deleteNode();
-                  }}
-                >
-                  Delete Task
-                </Button>
-              </SheetFooter>  
-            </div>
-          </SheetContent>
+                </SheetFooter>  
+              </div>
+            </SheetContent>
+          </form>  
         </Form>
       </Sheet>
     </>
